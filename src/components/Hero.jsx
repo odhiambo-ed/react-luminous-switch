234be4ed8cdcd182a5ea29e3501f42e46a259452
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-12 12:26:41 +0300

    setup hero content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,27 +5,25 @@ function Hero() {
       <div>
         <div
           className="d-sm-flex align-items-center justify-content-between w-100"
-          style="height: 100vh;"
+          style={{ height: "100vh" }}
         >
-          <div className="col-md-4 mx-auto mb-4 mb-sm-0 headline">
-            <span className="text-secondary text-uppercase">Subheadline</span>
+          <div className="col-md-4 mx-auto mb-4 mb-sm-0 headline px-4">
+            <span className="text-secondary text-uppercase">
+              LuminousSwitch
+            </span>
             <h1 className="display-4 my-4 font-weight-bold">
-              Enter Your <span style="color: #9B5DE5;">Headline Here</span>
+              Toggle to
+              <span style={{ color: "#9B5DE5" }}> Switch</span>
             </h1>
-            <a
-              href="#"
-              className="btn px-5 py-3 text-white mt-3 mt-sm-0"
-              style="border-radius: 30px; background-color: #9B5DE5;"
-            >
+            <div className="btn btn-primary px-5 py-3 text-white mt-3 mt-sm-0">
               Get Started
-            </a>
+            </div>
           </div>
           <div className="col-md-8 h-100 clipped hero-img"></div>
         </div>
         <a href="https://componentity.com" target="_blank" className="block">
           <img
-            src="http://codenawis.com/componentity/wp-content/uploads/2020/08/logo-componentity-%E2%80%93-9.png"
-            width="120px"
+            src="https://static.vecteezy.com/system/resources/thumbnails/012/986/755/small/abstract-circle-logo-icon-free-png.png"
             className="d-block mx-auto my-5"
           />
         </a>
