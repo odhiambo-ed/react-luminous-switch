41637466c926f47f1ae7abf85379487f2f1c7e59
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-12 12:48:09 +0300

    add props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import './App.css'
-import Hero from './components/Hero'
-import Nav from './components/Nav'
+import { useState } from "react";
+import "./App.css";
+import Hero from "./components/Hero";
+import Nav from "./components/Nav";
 
 function App() {
+  const [dark, setDark] = useState(false);
 
+  const toggleDark = () => {
+    setDark(!dark);
+  };
   return (
-    <>
-      <Nav />
+    <div
+      className={`${dark ? "dark" : ""}`}
+      data-bs-theme={`${dark ? "dark" : ""}`}
+    >
+      <Nav dark={dark} toggleDark={toggleDark} />
       <Hero />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
