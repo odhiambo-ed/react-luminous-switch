3042344c6e7dd4292560e679b71e65198ea30df2
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-12 10:49:51 +0300

    add toggle state and function

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,25 @@
-
+import { useState } from "react";
+import { BsToggleOn } from "react-icons/bs";
+import { BsToggleOff } from "react-icons/bs";
 
 function Nav() {
+    const [dark, setDark] = useState(false)
+
+    const toggleDark = () => {
+        setDark(!dark)
+    }
   return (
     <nav className="navbar bg-body-tertiary">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           Luminous Switch
         </a>
+        <span onClick={toggleDark}>
+          {dark ? <BsToggleOn size={"25px"} /> : <BsToggleOff size={"25px"} />}
+        </span>
       </div>
     </nav>
   );
 }
 
-export default Nav
+export default Nav;
